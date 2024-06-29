cd42d43932c8209cc342e27693a6ef3412aa8f38
Fuadi15 <muhammadnurfuadi778@gmail.com>
2024-06-29 05:51:49 -0700

    Initial commit

---
+++ profile.php
@@ -0,0 +1,53 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="text-center">
+            <img src="img/nobita.png" alt="Nobita" width="150" height="170" class="rounded-circle">
+            <table class="table mt-4" style="width:100%; margin:0 auto;">
+                <tbody>
+                    <tr>
+                        <td align="right" width="50%"><strong>NIK / NPM :</strong></td>
+                        <td align="left" width="50%">13 0520 205 / 2210010418</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Fakultas / Program Studio :</strong></td>
+                        <td align="left" width="50%">Teknologi Informasi / Teknik Informatika</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Nama Lengkap :</strong></td>
+                        <td align="left" width="50%">Muhammad NurFuadi</td> 
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Tempat, Tanggal Lahir :</strong></td>
+                        <td align="left" width="50%">Banjarmasin, 15 Mei 2004</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Telepon :</strong></td>
+                        <td align="left" width="50%">0832352725266</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>email :</strong></td>
+                        <td align="left" width="50%">muhammadnurfuadi15@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Media Sosial :</strong></td>
+                        <td align="left" width="50%">
+                            <a href="" target="_blank" style="text-decoration:none;">
+                                <img src="img/facebook.png" width="30" alt="facebook">
+                            </a>
+                            <a href="https://www.instagram.com/muh.fuad__" target="_blank" style="text-decoration:none;">
+                                <img src="img/instagram.png" width="30" alt="instagram">
+                            </a>
+                            <a href= target="_blank" style="text-decoration:none;">
+                                <img src="img/twitter.png" width="30" alt="twitter">
+                            </a>
+                            <a href="" target="_blank">
+                                <img src="img/linkedin.png" width="30" alt="linkedin">
+                            </a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<?php include "footer.php" ?>
